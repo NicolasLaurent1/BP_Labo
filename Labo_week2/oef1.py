d6a517969ee29cd5e9497d15e4185978953c2fd6
NicolasLaurent1 <nicolas.laurent@student.howest.be>
2017-10-02 11:07:05 +0200

    Een tweede wijzing

---
+++ Labo_week2/oef1.py
@@ -0,0 +1,7 @@
+getalA = int(input("Geef een getal"))
+getalB = int(input("Geef een tweede getal"))
+
+if(getalA == getalB):
+    print("Het getal {0} en het getal {1} zijn gelijk".format(getalA, getalB))
+else:
+    print("Het getal {0} en het getal {1} zijn verschillend".format(getalA, getalB))
